60788484e957fbaf649bafeaa8af39a3b2627cc1
Matt Casali <mattcasali1@gmail.com>
2020-09-05 18:20:02 -0600

    More updates, lesson 7

--- blogging/views.py
+++ blogging/views.py
@@ -6,8 +6,10 @@ from blogging.models import Post
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
-    return render(request, 'list.html', context)
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -18,3 +20,12 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
